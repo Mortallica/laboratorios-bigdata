c0f3c2af86ef69231b98bae5784156ac8412f594
Santiago Molina <santi.lds17@gmail.com>
2020-11-28 18:37:43 -0500

    punto 2 map-reduce

--- laboratorio Map-Reduce/salario-empleado.py
+++ laboratorio Map-Reduce/salario-empleado.py
@@ -9,15 +9,15 @@ class MRWordFrequencyCount(MRJob, ABC):
 
     def mapper(self, _, line):
         palabras = line.split(',')
-        sector = []
+        empleado = []
 
         for palabra_empleado in islice(palabras, 0, None, 4):
             for palabra_salary in islice(palabras, 2, None, 4):
                 if palabra_salary != 'salary' and palabra_empleado != 'idemp':
-                    sector.append([palabra_empleado, int(palabra_salary)])
+                    empleado.append([palabra_empleado, int(palabra_salary)])
 
         # x[0] = empleado  x[1] = salary
-        for x in sector:
+        for x in empleado:
             yield x[0], x[1]
 
     def reducer(self, key, values):
